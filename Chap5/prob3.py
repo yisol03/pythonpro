b2c24c9e56266070acec680af6b8b9d15ed65185
Yisol <ghkddlthf123@naver.com>
2023-10-11 00:52:32 +0000

    2023 10 11

--- Chap5/prob3.py
+++ Chap5/prob3.py
@@ -5,19 +5,31 @@ words = ('difficult', 'banana', 'apple', 'python', 'daegu', 'catholic', 'univers
 print("Guess the Word!!!")
 print("In this game, the program selects a word at random, and the player's objective is to guess the chosen word.\n")
 
-att = random.randrange(0, 8)
-word = words[att]
+word = random.choice(words)
 n = len(word)
-correct = [n]
+
+guessed = []
+
 print("Length of the selected word: ", n)
+
 while n > 0:
-    print("Remaining attempts: ", n)
-    for i in word:
-        print("_ ", end = " ")
-    
-    guess = input("Guess a letter: ")
-    if guess in word:
-        for i in word:
-            if word[i] == guess:
-                
-    
+	if all(letter in guessed for letter in word):
+		print("Congratulations! You guessed the word: ", word)
+		break
+
+	print("Remaining attempts: ", n)
+	dword = ''.join([letter if letter in guessed else '_ ' for letter in word])
+	print("Current guessed word: ", dword)
+	guess = input("Guess a letter: ")
+
+	if len(guess) == 1 and guess.isalpha() and guess not in guessed:
+		guessed.append(guess)
+
+	if guess in word:
+		continue
+	else:
+		print("Incorrect guess.")
+		n -= 1
+
+if n == 0:
+	print("You've used up all your attempts. The correct word was", word)
