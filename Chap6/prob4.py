9d4cf26a8c27848cb2f7e68dc2333a947ac8e6b8
yisol <ghkddlthf123@naver.com>
2023-10-25 15:26:40 +0900

    2023 10 25

--- Chap6/prob4.py
+++ Chap6/prob4.py
@@ -1,50 +1,90 @@
 import random
 
-# 사용할 단어 목록
-words = ["apple", "banana", "cherry", "date", "fig", "grape", "kiwi", "lemon", "mango", "orange"]
+def draw_hangman(wrong_attempts):
+    hangman = [
+        "  ____",
+        "  |  |",
+        "  |",
+        "  |",
+        "  |",
+        "  |",
+        "  |",
+        " / \\",
+        "/   \\",
+    ]
 
-# 무작위로 단어 선택
+    if wrong_attempts == 0:
+        return hangman[:3] + ["  |"] * 5
+    elif wrong_attempts == 1:
+        return hangman
+    elif wrong_attempts == 2:
+        hangman[2] = "  |  O"
+        return hangman
+    elif wrong_attempts == 3:
+        hangman[3] = "  |  |"
+        return hangman
+    elif wrong_attempts == 4:
+        hangman[4] = "  | /|"
+        return hangman
+    elif wrong_attempts == 5:
+        hangman[4] = "  | /|\\"
+        return hangman
+    elif wrong_attempts == 6:
+        hangman[5] = "  | /"
+        return hangman
+    elif wrong_attempts == 7:
+        hangman[5] = "  | / \\"
+        return hangman
+
 def choose_word():
-    return random.choice(words)
+    words = [ 'difficult', 'banana', 'apple', 'python', 'daegu', 'catholic', 'university' ]
+    return random.choice(words).upper()
 
-# 게임 실행
-def hangman():
-    word = choose_word()
-    guessed_letters = []
-    tries = 6  # 틀릴 수 있는 횟수
-
-    print("Welcome to Hangman!")
-    
-    while True:
-        display_word = ""
-        for letter in word:
-            if letter in guessed_letters:
-                display_word += letter
-            else:
-                display_word += "_"
-        
-        print("Word: " + display_word)
-        print("Tries left: " + str(tries))
-        
-        guess = input("Guess a letter: ").lower()
-        
-        if guess in guessed_letters:
-            print("You already guessed that letter.")
-        elif guess in word:
-            guessed_letters.append(guess)
-            print("Good guess!")
+def display_word(word, guessed_letters):
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter
         else:
-            guessed_letters.append(guess)
-            print("Wrong guess!")
-            tries -= 1
-        
-        if "_" not in display_word:
-            print("Congratulations! You've guessed the word: " + word)
-            break
-        
-        if tries == 0:
-            print("Out of tries! The word was: " + word)
-            break
+            display += "_"
+    return display
+
+def main():
+    word_to_guess = choose_word()
+    guessed_letters = []
+    max_attempts = 7
+    wrong_attempts = 0
+
+    while True:
+        print("\n" + " ".join(guessed_letters))
+        print("\n" + display_word(word_to_guess, guessed_letters))
+
+        guess = input("Enter your guess: ").upper()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("올바른 글자를 입력하세요.")
+            continue
+
+        if guess in guessed_letters:
+            print("이미 추측한 글자입니다.")
+            continue
+
+        guessed_letters.append(guess)
+
+        if guess in word_to_guess:
+            if set(word_to_guess) == set(guessed_letters):
+                print(f"축하합니다! 정답은 '{word_to_guess}'입니다.")
+                break
+        else:
+            wrong_attempts += 1
+            print("틀렸습니다.")
+            if wrong_attempts == max_attempts:
+                print("게임 오버! 단어는 '{word_to_guess}'였습니다.")
+                break
+
+        hangman_display = draw_hangman(wrong_attempts)
+        for line in hangman_display:
+            print(line)
 
 if __name__ == "__main__":
-    hangman()
+    main()
